b8d015cb0be1386e7ba7e90b80b8c1e7ed3b042c
zhagot <zhagot@mail.ru>
2021-04-12 12:35:51 +0300

    ДЗ 2 сделано.

--- src/ru/zhagot/ls2/Main.java
+++ src/ru/zhagot/ls2/Main.java
@@ -2,10 +2,35 @@ package ru.zhagot.ls2;
 
 public class Main {
     public static void main(String[] args) {
-        int[] myArr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.println("myArr.length: " + myArr.length);
-        for (int i = 0; i < myArr.length; i++) {
-            System.out.print(myArr[i] + " ");
+
+
+        convNumbs(); // ДЗ 1.
+        arrFully();
+    }
+        private static void convNumbs() { // ДЗ 1.
+        int[] arrNumbers = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Глубина массива: " + arrNumbers.length);
+        for (int i = 0; i < arrNumbers.length; i++) {
+            System.out.println("arrNumbers[" + i + "] = " + arrNumbers[i] + " - до изменения");
+            if (arrNumbers[i] == 0) {
+                arrNumbers[i] = 1;
+            }
+            if (arrNumbers[i] == 1) {
+                arrNumbers[i] = 0;
+            }
+            System.out.println("arrNumbers[" + i + "] = " + arrNumbers[i] + " - после изменения");
+            System.out.println();
+        }
+    }
+//    Задать пустой целочисленный массив размером 8.
+//    С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    private static void arrFully () {
+        int x = 0;
+        int[] arrFull = new int[8];
+        for (int i = 0; i < arrFull.length; i++) {
+            arrFull[i] = x;
+            x += 3;
+            System.out.println(arrFull[i]);
         }
     }
 }
