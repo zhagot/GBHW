0b177274595307efc2f804cf6ed782fd669ec366
zhagot <zhagot@mail.ru>
2021-04-12 06:55:52 +0300

    Начинаем делать ДЗ

--- src/ru/zhagot/ls2/Main.java
+++ src/ru/zhagot/ls2/Main.java
@@ -2,6 +2,10 @@ package ru.zhagot.ls2;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] myArr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("myArr.length: " + myArr.length);
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.print(myArr[i] + " ");
+        }
     }
 }
