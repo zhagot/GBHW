8074d7201e9e2b462595b6a5631082cef31818ba
zhagot <zhagot@mail.ru>
2021-05-27 02:57:43 +0300

    ДЗ по 5 уроку готово. Фикс. Удалил пустые классы.

--- src/ru/zhagot/ls5/Main.java
+++ src/ru/zhagot/ls5/Main.java
@@ -6,12 +6,8 @@ public class Main {
     Employee[] persArray = new Employee[5];
         persArray[0] = new Employee("Хубаев Давид", "Саппорт", "websix@vk.com", 89991115577l, 50, 30);
         persArray[1] = new Employee("Zhagot Denis", "Support", "zhagot@zhagot.ru", 89991115577l, 50, 37);
-        persArray[2] = new Employee("Бударих Владимир>", "Саппорт", "vova@vk.com", 89991115577l, 60, 25);
+        persArray[2] = new Employee("Бударин Владимир>", "Саппорт", "vova@vk.com", 89991115577l, 60, 25);
         persArray[3] = new Employee("Григорьев Илья", "Саппорт", "grig@vk.com", 89991115577l, 50, 31);
         persArray[4] = new Employee("Антощентов Павел", "Админ", "tov_marshal@vk.com", 89991115577l, 70, 31);
-
-
     }
-
-
 }
